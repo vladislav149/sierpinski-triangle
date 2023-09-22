d19ef9f6816d9130e34f3edcc3c3ffe6152c5a72
vladislav149 <vladislav149@mail.ru>
2023-09-23 01:42:38 +0700

    added feature add new top and delete new top

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
 import type {Point} from '@/types'
 import SerTriangle from '@/components/SerTriangle.vue'
 import {randomInteger} from '@/helpers'
-import {ref, computed, reactive} from 'vue'
+import {ref, computed, reactive, watch} from 'vue'
 
 let interval: ReturnType<typeof setInterval>
 const speedReal = ref(-150)
-const speed = computed(() => +speedReal.value + 210)
-const disabledRange = ref(false)
+const speed = computed(() => +speedReal.value + 200)
+const isDisabledRange = ref(false)
+const isDisabledStart = computed(() => {
+  return (
+    isDisabledRange.value ||
+    coefficient.value <= 1 ||
+    triangleTops.value.length < 3
+  )
+})
 const countPoints = computed(() => points.length)
+const coefficient = ref(2)
 const triangleTops = ref<Array<Point>>([
   {
     x: 250,
@@ -32,23 +40,27 @@ const points: Array<Point> = reactive([
 ])
 
 function addNewPoint() {
-  const number = randomInteger(0, 2)
+  const number = randomInteger(0, triangleTops.value.length - 1)
   const lastPoint = points.at(-1)
   if (!lastPoint) return
-  const x = (triangleTops.value[number].x + lastPoint.x) / 2
-  const y = (triangleTops.value[number].y + lastPoint.y) / 2
+  const x = Math.round(
+    (triangleTops.value[number].x + lastPoint.x) / coefficient.value
+  )
+  const y = Math.round(
+    (triangleTops.value[number].y + lastPoint.y) / coefficient.value
+  )
   points.push({x, y})
 }
 
 function startAddingNewPoints() {
-  disabledRange.value = true
+  isDisabledRange.value = true
   interval = setInterval(() => {
     addNewPoint()
   }, Math.abs(speedReal.value))
 }
 
 function stopAddingNewPoints() {
-  disabledRange.value = false
+  isDisabledRange.value = false
   clearInterval(interval)
 }
 
@@ -65,6 +77,25 @@ function changePosition(index: number, positionX: number, positionY: number) {
   triangleTops.value[index].x = positionX
   triangleTops.value[index].y = positionY
 }
+
+function addTop() {
+  const x = randomInteger(0, 500)
+  const y = randomInteger(0, 500)
+  triangleTops.value.push({x, y})
+}
+
+function deleteTop() {
+  triangleTops.value.splice(-1)
+}
+
+watch(coefficient, () => clearNewPoints())
+watch(
+  () => triangleTops.value.length,
+  () => {
+    stopAddingNewPoints()
+    clearNewPoints()
+  }
+)
 </script>
 
 <template>
@@ -83,14 +114,22 @@ function changePosition(index: number, positionX: number, positionY: number) {
       <label class="block">
         скорость
         <input
-          class="cursor-pointer"
+          class="cursor-pointer disabled:cursor-not-allowed"
           type="range"
           v-model="speedReal"
-          min="-200"
+          min="-190"
           max="0"
-          :disabled="disabledRange"
+          :disabled="isDisabledRange"
         />
         {{ speed }}
+      </label>
+      <label>
+        коэффициент 1/
+        <input
+          class="w-10"
+          type="number"
+          v-model="coefficient"
+        />
       </label>
       <SerTriangle
         class="mx-auto"
@@ -99,9 +138,10 @@ function changePosition(index: number, positionX: number, positionY: number) {
         @changePosition="changePosition"
       />
       <button
-        class="bg-teal-600 text-white px-5 py-10"
+        class="bg-teal-600 text-white px-5 py-10 disabled:cursor-not-allowed disabled:opacity-50 transition-opacity"
         type="button"
         @click="startAddingNewPoints"
+        :disabled="isDisabledStart"
       >
         Запустить
       </button>
@@ -118,6 +158,20 @@ function changePosition(index: number, positionX: number, positionY: number) {
         @click="clearNewPoints"
       >
         Очистить
+      </button>
+      <button
+        class="bg-green-600 text-white px-5 py-10"
+        type="button"
+        @click="addTop"
+      >
+        Добавить вершину
+      </button>
+      <button
+        class="bg-red-600 text-white px-5 py-10"
+        type="button"
+        @click="deleteTop"
+      >
+        Удалить вершину
       </button>
     </main>
     <footer></footer>
